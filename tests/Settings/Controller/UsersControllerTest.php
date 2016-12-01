5dba5d0a78b5381613d1a874669083c41756eb3a
Morris Jobke <hey@morrisjobke.de>
2016-12-02 00:23:33 +0100

    fix unit tests
    Signed-off-by: Morris Jobke <hey@morrisjobke.de>

--- tests/Settings/Controller/UsersControllerTest.php
+++ tests/Settings/Controller/UsersControllerTest.php
@@ -2491,7 +2491,7 @@ class UsersControllerTest extends \Test\TestCase {
 			->method('getUID')
 			->will($this->returnValue('def'));
 		$this->userSession
-			->expects($this->exactly(2))
+			->expects($this->exactly(1))
 			->method('getUser')
 			->will($this->returnValue($user));
 		$this->userManager
@@ -2500,14 +2500,9 @@ class UsersControllerTest extends \Test\TestCase {
 			->with('abc')
 			->willReturn(null);
 
-		$subadmin = $this->createMock('\OC\SubAdmin');
-		$subadmin->expects($this->once())
-			->method('isUserAccessible')
-			->will($this->returnValue(true));
 		$this->groupManager
-			->expects($this->once())
-			->method('getSubAdmin')
-			->willReturn($subadmin);
+			->expects($this->never())
+			->method('getSubAdmin');
 
 		$expectedResponse = new DataResponse(
 			[
@@ -2707,7 +2702,7 @@ class UsersControllerTest extends \Test\TestCase {
 			->method('getUID')
 			->will($this->returnValue('def'));
 		$this->userSession
-			->expects($this->exactly(2))
+			->expects($this->exactly(1))
 			->method('getUser')
 			->will($this->returnValue($user));
 		$this->userManager
@@ -2716,14 +2711,9 @@ class UsersControllerTest extends \Test\TestCase {
 			->with('abc')
 			->willReturn(null);
 
-		$subadmin = $this->createMock('\OC\SubAdmin');
-		$subadmin->expects($this->once())
-			->method('isUserAccessible')
-			->will($this->returnValue(true));
 		$this->groupManager
-			->expects($this->once())
-			->method('getSubAdmin')
-			->willReturn($subadmin);
+			->expects($this->never())
+			->method('getSubAdmin');
 
 		$expectedResponse = new DataResponse(
 			[
